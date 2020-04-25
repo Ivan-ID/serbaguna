45280303fed25d1854bcfe7332d7862682194798
Fake Smile <64054240+Ivan-ID@users.noreply.github.com>
2020-04-25 23:46:15 +0700

    Create email.py

---
+++ sc/email.py
@@ -0,0 +1,14 @@
+from requests import ConnectionError
+from time import sleep
+import requests, sys, os, re, random
+
+def Spam():
+        os.system('clear')
+        sleep(1.5)
+        os.system('xdg-open https://10minutemail.net/m/?lang=id')
+        os.system('clear')
+        sleep(1.3)
+        print
+
+if __name__ == '__main__':
+        Spam()
